cf78eb25a515860e078b80c99f253a1dc4874771
Slimbentanfous1 <slimbentanfous2002@gmail.com>
2024-04-28 22:19:57 +0100

    Including SMS Api Twilio Working(User)

--- src/main/java/tn/PiFx/controllers/RegistrationController.java
+++ src/main/java/tn/PiFx/controllers/RegistrationController.java
@@ -3,32 +3,22 @@ package tn.PiFx.controllers;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-
 import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Connection;
-import java.net.URL;
 import java.util.Optional;
 import java.util.Random;
-import java.util.ResourceBundle;
-
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import tn.PiFx.entities.User;
 import tn.PiFx.services.ServiceUtilisateurs;
-import tn.PiFx.utils.DataBase;
-import tn.PiFx.utils.SessionManager;
+
 
 public class RegistrationController {
 
@@ -63,9 +53,9 @@ public class RegistrationController {
     private final ServiceUtilisateurs UserS = new ServiceUtilisateurs();
 
     // Api SMS Slim
-    public static final String ACCOUNT_SID = "AC19cce70cb19c2d9ece20819b3722d89f";
-    public static final String AUTH_TOKEN = "b933ef29635257e886667b46e5c41b30";
-    public static final String TWILIO_PHONE_NUMBER = "+441571597017";
+    public static final String ACCOUNT_SID = "AC8e6265824899b900397db47f1bd6c4a1";
+    public static final String AUTH_TOKEN = "6cc028d0bc1f4682935e5d5da5599dad";
+    public static final String TWILIO_PHONE_NUMBER = "+16812026037";
     public String verificationCode;
 
     public String generateVerificationCode() {
@@ -90,7 +80,8 @@ public class RegistrationController {
     }
 
     @FXML
-    public void ConfirmerInscButton(javafx.event.ActionEvent actionEvent) {
+    public void ConfirmerInscButton(javafx.event.ActionEvent actionEvent)throws SQLException {
+
         int CIN = Integer.parseInt(CinInsTF.getText());
         String NOM = NomInscTf.getText();
         String PRENOM = PrenomInscTf.getText();
@@ -107,7 +98,10 @@ public class RegistrationController {
             } else if (UserS.checkUserExists(EMAIL)) {
                 reginfo.setText("Email déjà existe");
             } else {
+                System.out.println("CIN from TextField: " + CinInsTF.getText());
+
                 this.verificationCode = generateVerificationCode();
+                System.out.println("Gernerated Code: " + generateVerificationCode());
                 sendVerificationCode(String.valueOf(NUMTEL), this.verificationCode);
                 boolean isCodeVerified = false;
                 while (!isCodeVerified) {
@@ -122,7 +116,7 @@ public class RegistrationController {
                             isCodeVerified = true;
                             UserS.Add(new User(0,CIN, NOM, PRENOM, EMAIL, ADRESSE, NUMTEL, MDP, "User"));
                             try {
-                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/AdminUser.fxml"));
                                 Parent root = loader.load();
                                 Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
                                 Scene scene = new Scene(root);
@@ -148,17 +142,6 @@ public class RegistrationController {
             alert.setTitle("SQL Exception");
             alert.setContentText(e.getMessage());
             alert.showAndWait();
-        }
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Register.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.setTitle("Nova");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
